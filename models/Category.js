5c585bd40c7cba4641f25ddef8f0c1f15847fafd
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-06-23 17:47:09 -0500

    fixed const sequelize reference in all model files

--- models/Category.js
+++ models/Category.js
@@ -1,18 +1,18 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('sequelize');
-const Chore = require('./Chore');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const Chore = require("./Chore");
 
 class Chore extends Model {}
 
 Chore.init({
-	id: {
-		type: DataTypes.INTEGER,
-		primaryKey: true,
-		autoIncrement: true,
-	},
-	name: {
-		type: DataTypes.STRING,
-	},
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
 });
 
 module.exports = Chore;
--- models/Chore.js
+++ models/Chore.js
@@ -1,36 +1,36 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('sequelize');
-const Task = require('./Task');
-const Category = require('./Category');
-const Recurring_Pattern = require('./Recurring_Pattern');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const Task = require("./Task");
+const Category = require("./Category");
+const Recurring_Pattern = require("./Recurring_Pattern");
 
 class Chore extends Model {}
 
 Chore.init({
-	id: {
-		type: DataTypes.INTEGER,
-		primaryKey: true,
-		autoIncrement: true,
-	},
-	name: {
-		type: DataTypes.STRING,
-	},
-	category_id: {
-		type: DataTypes.INTEGER,
-		references: {
-			model: 'category',
-			key: 'id',
-		},
-	},
-	is_recurring: {
-		type: DataTypes.BOOLEAN,
-	},
-	recurring_pattern_id: {
-		references: {
-			model: 'Recurring_Pattern',
-			key: 'id',
-		},
-	},
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "category",
+      key: "id",
+    },
+  },
+  is_recurring: {
+    type: DataTypes.BOOLEAN,
+  },
+  recurring_pattern_id: {
+    references: {
+      model: "Recurring_Pattern",
+      key: "id",
+    },
+  },
 });
 
 module.exports = Chore;
