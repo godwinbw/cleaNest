9af8633b153ddd3c5c551c9730427770a6f6cdd6
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-06-23 18:15:48 -0500

    updated models to include sequelize info

--- models/Chore.js
+++ models/Chore.js
@@ -6,31 +6,39 @@ const Recurring_Pattern = require("./Recurring_Pattern");
 
 class Chore extends Model {}
 
-Chore.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-  },
-  category_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "category",
-      key: "id",
+Chore.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
+    is_recurring: {
+      type: DataTypes.BOOLEAN,
+    },
+    recurring_pattern_id: {
+      references: {
+        model: "Recurring_Pattern",
+        key: "id",
+      },
     },
   },
-  is_recurring: {
-    type: DataTypes.BOOLEAN,
-  },
-  recurring_pattern_id: {
-    references: {
-      model: "Recurring_Pattern",
-      key: "id",
-    },
-  },
-});
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "chore",
+  }
+);
 
 module.exports = Chore;
