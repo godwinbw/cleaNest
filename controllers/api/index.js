bae6b1aa9bfe9bab88bd1b11bcb0c7656aff8142
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-06-23 17:11:52 -0500

    merged feature/db

---
+++ controllers/api/index.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+const userRoutes = require("./user-routes.js");
+const categoryRoutes = require("./category-routes.js");
+const choreRoutes = require("./chore-routes.js");
+const patternRoutes = require("./pattern-routes.js");
+const taskRoutes = require("./task-routes.js");
+
+router.use("/users", userRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/chores", choreRoutes);
+router.use("/patterns", patternRoutes);
+router.use("/tasks", taskRoutes);
+
+module.exports = router;
