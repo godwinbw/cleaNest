41fc4c0c928f83912e051bbb4c574d1e96908745
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-06-23 19:30:10 -0500

    workin on api routes

--- models/index.js
+++ models/index.js
@@ -1,15 +1,10 @@
-const User = require("./User");
-const Task = require("./Task");
-const Chore = require("./Chore");
 const Category = require("./Category");
 const Recurring_Pattern = require("./Recurring_Pattern");
+const Chore = require("./Chore");
+const Task = require("./Task");
+const User = require("./User");
 
-// *** USER associations ***
-//
-// a USER has many tasks
-User.hasMany(Task, {
-  foreignKey: "user_id",
-});
+/*
 
 // a TASK belongs to one user
 Task.belongsTo(User, {
@@ -17,17 +12,11 @@ Task.belongsTo(User, {
   onDelete: "SET NULL",
 });
 
-// *** TASK associations
-//
-// a TASK is a type a single CHORE
+// a CHORE belongs to a TASK
 Task.hasOne(Chore, {
   foreignKey: "chore_id",
 });
 
-// a CHORE can have many TASKS
-Chore.hasMany(Task, {
-  foreignKey: "chore_id",
-});
 
 // *** CHORE associations
 //
@@ -37,17 +26,9 @@ Chore.hasOne(Category, {
   foreignKey: "category_id",
 });
 
-// a CATEGORY can have many CHORES
-Category.hasMany(Chore, {
-  foreignKey: "category_id",
-});
-
 // a CHORE has one RECURRING PATTERN
 Chore.hasOne(Recurring_Pattern, {
   foreignKey: "recurring_pattern_id",
 });
 
-// a RECURRING PATTERN can have many CHORES
-Recurring_Pattern.hasMany(Chore, {
-  foreignKey: "recurring_pattern_id",
-});
+*/
