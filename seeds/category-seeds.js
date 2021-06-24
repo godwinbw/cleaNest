a8fb71afb9eb3d39d6bce120618ad5a6eb24bdf6
reinhardtc <reinhardtc22@gmail.com>
2021-06-23 20:32:17 -0400

    working on seed files

---
+++ seeds/category-seeds.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const { Chore } = require('../models');
+
+const category = [
+	{
+		name: 'Kitchen',
+	},
+	{
+		name: 'Bedroom',
+	},
+	{
+		name: 'Living Room',
+	},
+	{
+		name: 'Kitchen',
+	},
+	{
+		name: 'Kitchen',
+	},
+	{
+		name: 'Kitchen',
+	},
+];
---
+++ seeds/chore-seeds.js
@@ -0,0 +1,12 @@
+const sequelize = require('../config/connection');
+const { Task, Category } = require('../models');
+
+
+const chore = [
+    {
+        name: 'wash the dishes',
+        category_id: ,
+        is_recurring:
+        recurring_pattern_id: 
+    }
+]
