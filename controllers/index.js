86b20ce125b625ec90c38448e5abcc6e6380bcab
YajairaGracia <yygrace03@gmail.com>
2021-06-28 17:11:25 -0500

    login

--- controllers/index.js
+++ controllers/index.js
@@ -8,4 +8,5 @@ const apiRoutes = require("./api");
 //router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
 
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 
-//const exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,15 +18,19 @@ app.use(session(sessionConfig));
 
 //const helpers = require("./utils/helpers");
 
-//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ });
 
-//app.engine("handlebars", hbs.engine);
-//app.set("view engine", "handlebars");
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.get('/', (req, res) => {
 
+
+  res.render('homepage');
+});
 app.use(require("./controllers/"));
 
 sequelize.sync({ force: false }).then(() => {
